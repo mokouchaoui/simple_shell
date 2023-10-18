d775a409ed60da9b3adaaac352c598d23fae7a40
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-10-18 10:55:15 +0100

    fix b error

--- hsh_env.c
+++ hsh_env.c
@@ -2,32 +2,31 @@
 
 /**
  * hsh_env - builtin command hsh_env, mimics sh builtin env
- * @arg_list: argument list vectors
+ * @arg_list: arguement list vectors
  * @envp: environmental variables to print
  * Return: 0 if success.
  */
 int hsh_env(char **arg_list, env_t *envp)
 {
-    int i;
+	int i;
 
-    if (arg_list[1] == NULL)
-    {
-        print_env(envp);
-        return (0);
-    }
+	if (arg_list[1] == NULL)
+	{
+		print_env(envp);
+		return (0);
+	}
 
-    for (i = 1; arg_list[i] != NULL; i++)
-        arg_list[i - 1] = arg_list[i];
-    arg_list[i - 1] = arg_list[i];
-    return (1);
+	for (i = 1; arg_list[i] != NULL; i++)
+		arg_list[i - 1] = arg_list[i];
+	arg_list[i - 1] = arg_list[i];
+	return (1);
 }
-
 /**
  * hsh_env_help - builtin help printout for env
  * Return: Always 0
  */
 int hsh_env_help(void)
 {
-    _write("env usage: env\n    Prints out the current environment.\n");
-    return (0);
+	_write("env usage: env\n    Prints out the current envirornment.\n");
+	return (0);
 }
